969b4c3537d7d370f64de13ddf46ac5d34115eb0
Sam Marxz <sammarxzz@gmail.com>
2020-09-18 16:09:00 -0300

    add GUI.

--- 01-screenshot/app.py
+++ 01-screenshot/app.py
@@ -1,14 +1,41 @@
-import os
 import time
 import pyautogui
+import tkinter as tk
 
+from io import BytesIO
+from PIL import Image
+
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
 
 def screenshot():
     name = int(round(time.time() * 1000))
     name = "screenshots/{}.png".format(name)
+    root.iconify()
     time.sleep(3)
     img = pyautogui.screenshot(name)
-    img.show()
-    
+
+
+button = tk.Button(
+    frame,
+    text="Take Screenshot",
+    command=screenshot
+)
+
+button.pack(side = tk.LEFT)
+
+quit_button = tk.Button(
+    frame,
+    text="QUIT",
+    command=quit
+)
+
+quit_button.pack(side = tk.LEFT)
+
+root.title('Simple Screenshot Tool')
+root.mainloop()
+root.resizable(0,0)
 
 screenshot()
